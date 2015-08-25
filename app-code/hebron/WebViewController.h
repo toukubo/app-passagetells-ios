ff092d614bc79bcfa9a84707e6a557d7162ca5c1
ZhengXiaoMin <zheng@1120.com>
2015-08-26 04:33:15 +0800

    webview integration

---
+++ app-code/hebron/WebViewController.h
@@ -0,0 +1,13 @@
+//
+//  WebViewController.h
+//  passagetells
+//
+//  Created by HoneyPanda on 8/26/15.
+//  Copyright (c) 2015 Daisuke Nakazawa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewController : UIViewController
+
+@end
