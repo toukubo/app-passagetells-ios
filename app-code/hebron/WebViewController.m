090f6beb84b8eb41822c951cb6c9dc9678aea00f
TOKUBOKOJI <toukubo@gmail.com>
2015-08-29 03:08:28 +0900

    crash issue fixed.

--- app-code/hebron/WebViewController.m
+++ app-code/hebron/WebViewController.m
@@ -30,7 +30,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
     
     self.navigationController.navigationBarHidden = YES;
     
@@ -49,8 +48,6 @@
     
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:HOME_URL]]];
     
-
-    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -58,21 +55,17 @@
     // Dispose of any resources that can be recreated.
 }
 
--(void)resume
-{
-
+-(void)resume{
     if (self.needToReload) {
         [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:HOME_URL]]];
     }
 }
 
 -(bool)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation {
-    
     return (toInterfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
-{
+- (void)alertView:(UIAlertView *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
     NSLog(@"clickedButtonAtIndex: %lu", (unsigned long)buttonIndex);
     if(buttonIndex == 0) {
         // "retry" selected: try to load "Top" page
@@ -86,9 +79,6 @@
 
 - (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request
  navigationType:(UIWebViewNavigationType)navigationType {
-    
-    
-    
     
     NSLog(@"passagetells//");
     NSString *actionsAndParams = request.URL.lastPathComponent;
@@ -109,24 +99,42 @@
         [queryStringDictionary setObject:value forKey:key];
     }
     
-    
     NSLog(actionsAndParams);
     NSLog(query);
-
-    
-    
-    
     
     NSLog(@"shouldStartLoadWithRequest: %@", [request.URL lastPathComponent]);
     if ([[request.URL scheme] isEqual:@"passagetells"]) { // TODO && uri contains download.
         
         NSLog(@"was path was...");
-        if ([actionsAndParams rangeOfString:@"download"].location != NSNotFound) {
+        if([actionsAndParams rangeOfString:@"selectProject"].location != NSNotFound) {
+            NSLog(@"passagetells///selectProject action");
+            
+            //parse the url
+            self.project_id = (NSString*)[queryStringDictionary objectForKey:@"id"];
+            self.project_name = (NSString*)[queryStringDictionary objectForKey:@"name"];
+            [[DataManager sharedManager] setProject_name:self.project_name];
+            NSLog(self.project_id);
+            NSLog(self.project_name);
+            NSLog(@"was the project id");
+            NSMutableString *projectURL = [NSMutableString stringWithString:HOME_URL];
+            [ projectURL appendString:@"/"];
+            [projectURL appendString:self.project_name];
+            [projectURL appendString:@"/intro.html"];
+            NSLog(projectURL);
+            NSString *oururl = [NSString stringWithString:projectURL];
+            NSLog(oururl);
+            NSURL* url = [NSURL URLWithString: oururl];
+            
+            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:30];
+            
+            [webView loadRequest:request];
+            
+            return NO;
+        }else if ([actionsAndParams rangeOfString:@"download"].location != NSNotFound) {
             NSLog(@"passagetells///download action");
             
             // Get JSON files
             // BeaconID.JSON
-            [[[DataManager sharedManager] beaconIDs] removeAllObjects];
             [MBProgressHUD showHUDAddedTo:self.view animated:YES];
             
             
@@ -179,62 +187,9 @@
             
             
             ///////////// downloading beacons
-            
-            NSMutableString *mutableBeaaconsURL = [NSMutableString stringWithString:HOME_URL];
-            [mutableBeaaconsURL appendString:@"/"];
-            
-            [mutableBeaaconsURL appendString:self.project_name];
-            [mutableBeaaconsURL appendString:@"/"];
-            NSString *ourbeaconsurl = [NSString stringWithString:mutableBeaaconsURL];
-            NSLog(ourbeaconsurl);
-            [[AFNetManager sharedManager] sendGETRequestTo:ourbeaconsurl path:@"beaconid.json" params:@{} success:^(id successBlock) {
-                
-                NSString *theJson= [[NSString alloc] initWithData:successBlock encoding:NSUTF8StringEncoding];
-                NSLog(theJson);
-                [[DataManager sharedManager] setBeaconID:theJson];
-                
-                NSDictionary *dict = [theJson JSONValue];
-                
-                for (NSString *key in [dict allKeys]) {
-                    NSString *value = [dict valueForKey:key];
-                    
-                    [[[DataManager sharedManager] beaconIDs] addObject:[[BeaconID alloc] initWith:[key intValue] mediaID:[value intValue]]];
-                }
-                self.beaconsIDSelf =                [[DataManager sharedManager] beaconIDs];
-                NSLog(@"yearh");
-                [self downloadMp3s];
-
-            } error:^(NSError *error) {
-                NSLog(@"Please check your internet connection.");
-                
-                [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
-            }];
-            
-            
-            
-
-            
-            
-        }else if([actionsAndParams rangeOfString:@"selectProject"].location != NSNotFound) {
-            NSLog(@"passagetells///selectProject action");
-            
-            //parse the url
-            self.project_id = (NSString*)[queryStringDictionary objectForKey:@"id"];
-            self.project_name = (NSString*)[queryStringDictionary objectForKey:@"name"];
-            [[DataManager sharedManager] setProject_name:self.project_name];
-            NSLog(self.project_id);
-            NSLog(self.project_name);
-            NSLog(@"was the project id");
-            NSMutableString *projectURL = [NSMutableString stringWithString:HOME_URL];
-                        [ projectURL appendString:@"/"];
-            [projectURL appendString:self.project_name];
-            [projectURL appendString:@"/intro.html"];
-            NSLog(projectURL);
-            NSString *oururl = [NSString stringWithString:projectURL];
-            NSLog(oururl);
-            NSURL* url = [NSURL URLWithString: oururl];
-            
-            NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:30];
+            if([[[DataManager sharedManager] beaconID] count]==0){
+                [[DataManager sharedManager] setBeaconID:[BeaconListner parseIntoDictionary]];
+            }
             
             
             
@@ -246,45 +201,30 @@
             CLBeaconRegion *region =         [[CLBeaconRegion alloc] initWithProximityUUID:proximityUUID identifier:@"EstimoteRegion"];
             
             [self.locationManager startRangingBeaconsInRegion:region];
+            
+            [self downloadMp3s];
 
+
+        }else  if([actionsAndParams rangeOfString:@"ok"].location != NSNotFound) {
             
-            
-            
-            [request setHTTPMethod:@"GET"];
-            [webView loadRequest:request];
-            
-            
-            
-            
-            //TODO save the project id and
-            //            [[UIApplication sharedApplication] openURL:oururl];
-            return NO;
-        }else if([actionsAndParams rangeOfString:@"ok"].location != NSNotFound) {
-     
             ViewController *vc = [self.storyboard instantiateViewControllerWithIdentifier:@"MainViewController"];
             [self.navigationController pushViewController:vc animated:YES];
-
+            
         }
         return NO;
     } else if ([[request.URL scheme] isEqual:@"mailto"] || [[request.URL scheme] isEqual:@"tel"]) {
         [[UIApplication sharedApplication] openURL:request.URL];
         return NO;
     } else {
-
+        
     }
     return YES;
-
+    
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView {
-    //NSLog(webView.request.URL.absoluteString);
-    
-    // get user id from URL string
     NSLog(@"webViewDidFinishLoad: %@", [webView.request.URL lastPathComponent]);
-    
-    // hide loading indicator
     if (mbLoad != nil && !mbLoad.isHidden) {
-        NSLog(@"hide loading indicator");
         [mbLoad hide:YES];
     }
 }
@@ -308,9 +248,7 @@
 
 #pragma mark -
 #pragma mark MBProgressHUDDelegate methods
-
 - (void)hudWasHidden:(MBProgressHUD *)hud {
-    // Remove HUD from screen when the HUD was hidded
     [mbLoad removeFromSuperview];
     mbLoad = nil;
 }
@@ -318,28 +256,19 @@
 #pragma mark -
 #pragma mark Download MP3s
 -(void)downloadMp3s{
-    
     NSMutableArray *savedFiles = [[DataManager sharedManager] mp3Files];
-    
     self.mp3FileArray = [[NSMutableArray alloc] init];
-    
     if ([savedFiles count] == 36) {
         [self gotoNextVC];
         return;
-    }
-    else if ([savedFiles count] == 0) {
+    }    else if ([savedFiles count] == 0) {
         self.mp3FileArray = [NSMutableArray arrayWithArray:self.beaconsIDSelf];
-    }
-    else
-    {
-        
+    }    else    {
         for (BeaconID *beacon in self.beaconsIDSelf) {
-            
-
             int thenumber =1*100+beacon.mediaID;
             NSString *downFile = [ NSString stringWithFormat:@"%04d%@", thenumber,@".mp3"];
             NSLog(downFile);
-
+            
             BOOL flag = false;
             
             for (Mp3File *savedFile in savedFiles) {
@@ -351,27 +280,17 @@
             
             if (flag) {
                 continue;
-            }
-            else
-            {
+            }        else        {
                 [self.mp3FileArray addObject:beacon];
             }
         }
     }
     
     [MBProgressHUD showHUDAddedTo:self.view animated:YES];
-    
-    //    for (NSString *url in self.mp3FileArray) {
-    //        [self downloadMp3File:[NSString stringWithFormat:@"%@%@",BASE_URL,url]];
-    //    }
-    
-    //1 hardcoded TODO
-
     if([self.mp3FileArray count] != 0){
         [self downloadMp3File:((BeaconID*)self.mp3FileArray[0]).mediaID];
     }else{
         [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
-
         [self gotoNextVC];
     }
 }
@@ -381,7 +300,7 @@
     int thenumber =1*100+mediaid;
     NSString *filename = [ NSString stringWithFormat:@"%04d%@", thenumber,@".mp3"];
     NSString *urlString = [NSString stringWithFormat:@"%@%@",BASE_URL, filename];
-
+    
     NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:urlString]];
     AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
     
@@ -403,11 +322,8 @@
             [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
             NSLog(urlString);
             [self gotoNextVC];
-        }
-        else
-        {
+        }        else        {
             [self downloadMp3File:((BeaconID*)self.mp3FileArray[0]).mediaID];
-
         }
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
@@ -418,21 +334,15 @@
             [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
             
             [self gotoNextVC];
-        }
-        else
-        {
+        }     else     {
             [self.mp3FileArray removeObjectAtIndex:0];
             if ([self.mp3FileArray count] == 0) {
                 [MBProgressHUD hideAllHUDsForView:self.view animated:YES];
-                
                 [self gotoNextVC];
-
+                
             }else{
                 [self downloadMp3File:((BeaconID*)self.mp3FileArray[0]).mediaID];
-                
             }
-
-
         }
     }];
     
@@ -443,7 +353,7 @@
 #pragma mark-
 #pragma mark Navigation Methods
 -(void)gotoNextVC{
-    
+    //    return ;
     [[DataManager sharedManager] saveManager];
     
     NSMutableString *projectURL = [NSMutableString stringWithString:HOME_URL];
@@ -453,67 +363,53 @@
     [projectURL appendString:self.project_name];
     [projectURL appendString:@"/slider.html#instructions"];
     if([[DataManager sharedManager] onsite]){
-//        [projectURL appendString:@"?onsite=1"];
+        [projectURL appendString:@"?onsite=1"];
     }
     NSLog(projectURL);
     NSString *oururl = [NSString stringWithFormat:@"%@",projectURL];
     NSLog(oururl);
+    
+    //    NSURL *url = [NSURL URLWithString: @"http://www.google.com"];
     NSURL *url = [NSURL URLWithString: oururl];
     
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.webView stopLoading];
-
+    //    [self.webView stopLoading];
+    
     [self.webView loadRequest:request];
-
+    //    [self.webView stopLoading];
+    
+    NSLog(@"and it is done");
     
     
 }
 
 
-
-/*
- Delicate method reciving beacons
- - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
- Parameters
- manager : The location manager object reporting the event.
- beacons : An array of CLBeacon objects representing the beacons currently in range. You can use the information in these objects to determine the range of each beacon and its identifying information.
- region  : The region object containing the parameters that were used to locate the beacons
- */
 #pragma mark - Location Manager
 - (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region {
-    NSLog(@"mark 3");
-    if([beacons count] == 0) { return; }
-    if([[[DataManager sharedManager] beaconID] count]==0){
-        [[DataManager sharedManager] setBeaconID:[BeaconListner parseIntoDictionary]];
+    NSLog(@"mark 3 location manager called.");
+    if(beacons ==nil){
+        NSLog(@"beacons are nil but didRange called");
+    }else{
+        NSLog(@"beacons are NOT nil and didRange called");
+        
+        
+        if([beacons count] == 0) { return; }
+        NSLog(@"beacons count is not 0");
+        NSLog(@"%d",[beacons count]);
+        if([[DataManager sharedManager] beaconID]==nil){
+            NSLog(@"and beacon id is null");
+        }
+        NSLog(@"%l",[[[DataManager sharedManager] beaconID] count]);
+        
+        CLBeacon *beacon = [ViewController getBeacon:beacons beaconID: [[DataManager sharedManager] beaconID]];
+        if(beacon!=nil){
+            [[DataManager sharedManager] setOnsite:TRUE];
+            NSLog(@"mark 5 beacon in the ids found. set on site. ");
+        }
+        
     }
-    CLBeacon *beacon = [ViewController getBeacon:beacons beaconID: [[DataManager sharedManager] beaconID]];
-    if(beacon!=nil){
-        [[DataManager sharedManager] setOnsite:TRUE];
-    }
-    int ii = -1, iii = 0;
-    //use the first one except unknown
-    for(int i = 0; i < [beacons count]; i++) {
-//        var beacon = beacons[i] as! CLBeacon
-//        var ttt = "\(beacon.minor):\(beacon.accuracy):\(beacon.rssi):"
-//        if (beacon.proximity == CLProximity.Unknown) {
-//            ttt += "Unknown\n"
-//        } else if (beacon.proximity == CLProximity.Immediate) {
-//            ttt += "Immediate\n"
-//        } else if (beacon.proximity == CLProximity.Near) {
-//            ttt += "Near\n"
-//        } else if (beacon.proximity == CLProximity.Far) {
-//            ttt += "Far\n"
-//        }
-//        if (beacon.proximity != CLProximity.Unknown && beaconID["\(beacon.major)\(beacon.minor)"] != nil && iii == 0) {
-//            ii = i  // save the first one's number
-//            t = ("\(ttt)\n") // save the first one's info
-//            iii = 1 // the flag that the first one is got
-//        }
-//        tt += ttt
-    }
-    //D/ self.beaconlist.text = t + tt;
-
-
+    
+    
 }
 
 
